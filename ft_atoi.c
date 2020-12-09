cefa840828b0840427bbe8b7fedc002726785bf0
adidion19 <didionantoine@gmail.com>
2020-12-09 11:55:49 +0100

    bon cette fois c'est la derniere inshallah

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: adidion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 09:09:45 by adidion           #+#    #+#             */
-/*   Updated: 2020/11/26 14:53:38 by adidion          ###   ########.fr       */
+/*   Updated: 2020/12/09 11:52:23 by adidion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,25 +25,25 @@ static long	ft_strtol(const char *str)
 	unsigned long long	i;
 	long long			ans;
 	int					neg;
+	int					j;
 
 	i = 0;
 	ans = 0;
 	neg = 1;
+	j = 0;
 	while (ft_isspace(str[i]) == 1)
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
-		if (str[i] == '-')
-			neg = neg * -1;
+		((str[i] == '-') && (neg = neg * -1));
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		ans = ans * 10;
-		ans = ans + str[i] - '0';
-		i++;
+		ans = ans * 10 + str[i++] - '0';
+		j++;
 	}
-	if (ans < 0)
+	if (ans < 0 || j > 19)
 		return (ans = (neg > 0) ? -1 : 0);
 	ans = ans * neg;
 	return ((long)ans);
--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: adidion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 16:09:20 by adidion           #+#    #+#             */
-/*   Updated: 2020/11/30 10:24:55 by adidion          ###   ########.fr       */
+/*   Updated: 2020/12/09 10:30:31 by adidion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memchr(const void *str2, int c, size_t n)
 	i = 0;
 	a = (unsigned char)c;
 	str = (unsigned char*)str2;
-	while ((str[i] && n > i) || c == 0)
+	while (n > i)
 	{
 		if (str[i] == a)
 			return (&str[i]);
